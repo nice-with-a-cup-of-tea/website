b244f72e167301e0b997297b76cce7b2877e8ee9
Flamanis <Flamanis@users.noreply.github.com>
2025-06-08 14:03:38 -0500

    Add node js adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 import preact from "@astrojs/preact";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://nicewithacupoftea.com",
-  integrations: [preact()]
+  site: "https://nice-with-a-cup-of-tea-a60a19e5af38.herokuapp.com/",
+  integrations: [preact()],
+  adapter: node( {
+    mode: 'standalone',
+  })
 });
